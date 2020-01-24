4d87cd2ef1244b9d9a02e5d6802eee01abb532ad
Kirkwood Williams <you@yourdomain>
2020-01-24 11:30:01 -0800

    Lab 2 step 1

--- bash/getpics.sh
+++ bash/getpics.sh
@@ -3,13 +3,10 @@
 # this script puts some picture files into a Pictures directory in our home directory
 # it does some error testing
 # it summarizes the Pictures directory when it is done
-#
+##
 
 # Task 1: Improve this script to also retrieve and install the files kept in the https://zonzorp.net/pics.tgz tarfile
 #   - use the same kind of testing that is already in the script to only download the tarfile if you don't already have it and  to make sure the download and extraction commands work, then delete the local copy of the tarfile if the extraction was successful
-########################################
-#make sure we have pictures directory
-#######################################
 
 # make a Pictures directory if we don't have one - assumes we have a home directory
 test -d ~/Pictures || mkdir ~/Pictures
@@ -21,19 +18,17 @@ test -f ~/Pictures/pics.zip || wget -q -O ~/Pictures/pics.zip http://zonzorp.net
 test -f ~/Pictures/pics.zip && unzip -d ~/Pictures -o -q ~/Pictures/pics.zip && rm ~/Pictures/pics.zip
 
 
-######################################
-#Summarize and report what we have in the pictures directory
-#######################################
+########################################
+# This script will be Retrieving and installing a tar file from the https://zonzorp.net/pics.tgz tarfile
 
-#handle the tar files
-#########################
-#download the tarfile if we need it
 test -f ~/Pictures/pics.tgz || wget -q -O ~/Pictures/pics.tgz http://zonzorp.net/pics.tgz
 
-#unpack the tarfile and remove it if successful
-test -f ~/Pictures/pics.tgz && tar && rm ~/Pictures/pics.tgz
+##########################################
+# This script will unpack the tar file
 
-#make a report on what we have in the pitcutre directory
+test -f ~/Pictures/pics.tgz && tar -xzf ~/Pictures/pics.tgz && rm ~/Pictures/pics.tgz
+
+# Make a report on what we have in the Pictures directory
 test -d ~/Pictures && cat <<EOF
 Found $(find ~/Pictures -type f|wc -l) files in the Pictures directory.
 The Pictures directory uses $(du -sh ~/Pictures|awk '{print $1}') space on the disk.
