86adc494fb25465f9bac3aec63e6cdd15968fea5
Kirkwood Williams <you@yourdomain>
2020-01-24 15:36:12 -0800

    Lab 2 rolling dice edit part 2

---
+++ bash/diceroller.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# this script rolls a six-sided die
+#
+
+# Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
+
+# roll the dice and display the result
+echo "
+Rolling...
+$(( RANDOM % 6 + 1)) rolled
+"
+########################################
+#Modify the script to be more descriptive of the output to tell the user you are rolling a pair of six-sided dice
+
+echo "
+You are rolling a pair of six-sided dice...
+$(( RANDOM % 6 + 1)) rolled
+"
+
+#Modify the script to add rolling 5 six-sided dice
+
+echo "
+You are rolling 5 six-sided dice...
+$(( RANDOM % 6 + 1))
+$(( RANDOM % 6 + 1))
+$(( RANDOM % 6 + 1))
+$(( RANDOM % 6 + 1))
+$(( RANDOM % 6 + 1)) 
+rolled
+"
+
+
+
+
+
+
+
+#######################################
+#Modify the script to add rolling one 20-sided die
